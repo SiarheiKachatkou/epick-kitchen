11463bcc28fabb44ba403c08047d807ff963f2fd
sergeykochetkov <sergeykochetkov@yandex.ru>
2020-11-15 13:14:18 +0300

    evaluate_implemented

---
+++ evaluate.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import torch
+
+def init_metrics(epochs):
+    nan_data = np.full((epochs + 1, 6), np.nan)
+    metrics = pd.DataFrame(data=nan_data,
+                           columns=['train_acc', 'val_acc', 'test_acc', 'train_loss', 'val_loss', 'test_loss'],
+                           copy=True)
+    
+    return metrics
+
+
+def evaluate(metrics,model,criterion,train_loader, val_loader, test_loader, epoch):
+
+    model.eval()
+
+    for column,loader in zip(['train','val','test'],[train_loader,val_loader,test_loader]):
+        losses=[]
+        accs=[]
+        for snippets,targets in loader:
+            logits=model(snippets)
+            loss=criterion(logits, targets)
+            losses.append(loss.detach().cpu().numpy())
+            preds=torch.argmax(logits,dim=1)
+            acc=torch.equal(targets,preds).mean()
+            accs.append(acc.detach().cpu().numpy())
+        metrics[column+'_acc'][epoch]=np.mean(accs)
+        metrics[column + '_loss'][epoch] = np.mean(losses)
+
+    return metrics
+
+
+
--- run_transfer_learning.py
+++ run_transfer_learning.py
@@ -11,12 +11,13 @@ from tqdm import tqdm
 from utils_snippets import clip_into_snippets, noun_verb_from_path
 from consts import height, width, segment_count, class_counts, repo, batch_size, \
     nouns, verbs, frames_path_pattern, heads, base_models, device, random_iters, augm_fn_list, \
-    fine_tune_verbs, fine_tune_val_split, fine_tune_epochs, fine_tune_head, fine_tune_base
+    fine_tune_verbs, fine_tune_val_split, fine_tune_epochs, fine_tune_head, fine_tune_base, fine_tune_lr
 from utils_visualization import show_snippet
 from utils_snippets import normalize_inputs_for_model
 from NarrowModel import NarrowModel
 from KitchenDataset import KitchenDataset
 from augmentations import get_4_augms_list, get_1_augms_list
+from evaluate import init_metrics,evaluate
 
 def get_filtered_paths(root_path, verbs_list):
     p=[]
@@ -25,6 +26,8 @@ def get_filtered_paths(root_path, verbs_list):
     return p
 
 if __name__=="__main__":
+
+    fine_tune_class_idxs=[np.where(verbs.class_key==v)[0][0] for v in fine_tune_verbs]
 
     train_val_video_paths = get_filtered_paths('data/frames_b',fine_tune_verbs)
     random.shuffle(train_val_video_paths)
@@ -38,8 +41,10 @@ if __name__=="__main__":
         model = torch.hub.load(repo, fine_tune_head, class_counts, segment_count, 'RGB',
                                base_model=fine_tune_base,
                                pretrained='epic-kitchens', force_reload=True)
-        model.eval()
+
+        model=NarrowModel(wide_model=model,verb_class_idxs=fine_tune_class_idxs)
         model.to(device)
+
     except:
         print(f'enable load {fine_tune_head} with {fine_tune_base}')
         raise NotImplementedError()
@@ -53,37 +58,21 @@ if __name__=="__main__":
     test_dataset=KitchenDataset(test_video_paths,height,width,segment_count,model,is_random=False,augm_fn=None)
     test_loader=DataLoader(test_dataset,batch_size=batch_size,shuffle=False)
 
-    metrics=pd.DataFrame()
+    metrics=init_metrics(fine_tune_epochs)
 
+    criterion=torch.nn.CrossEntropyLoss()
+
+    metrics=evaluate(metrics,model,criterion, train_loader, val_loader, test_loader, epoch=0)
+
+    optimizer = torch.optim.Adam(model.head.parameters(),lr=fine_tune_lr)
     for epoch in range(fine_tune_epochs):
+        model.train()
         for snippets,targets in train_loader:
+            optimizer.zero_grad()
+            snippets=snippets.to(device)
+            logits=model(snippets)
+            loss=criterion(logits,targets)
+            loss.backward()
+            optimizer.step()
 
-
-            for random_iter in tqdm(range(random_iters)):
-                for augm_fn in augm_fn_list:
-                    for src_video_path in src_video_paths:
-                        snippets = clip_into_snippets(src_video_path, height, width, segment_count, is_random=True,
-                                                      augm_fn=augm_fn_list)
-
-                        target_noun, target_verb = noun_verb_from_path(src_video_path)
-
-                        inputs = normalize_inputs_for_model(snippets, model)
-
-                        inputs = inputs.reshape((batch_size, -1, height, width))
-                        inputs = inputs.to(device)
-
-                        start = time.time()
-                        with torch.no_grad():
-                            verb_logits, noun_logits = model(inputs)
-                        finish = time.time()
-                        the_time = finish - start
-                        update_performance(model_performance, noun_logits, verb_logits, nouns, verbs, target_noun,
-                                           target_verb, the_time)
-
-                        # show_snippet(snippets)
-
-            finalize_performance(model_performance)
-            print(model_performance)
-            perfs.update({(head, base_model): model_performance})
-
-    plot_performance(perfs, base_models, heads)
+        metrics = evaluate(metrics, model, criterion, train_loader, val_loader, test_loader, epoch=epoch+1)
